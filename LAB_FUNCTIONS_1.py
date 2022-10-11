dd6666f5f998517f5fc3536db9f5e4410f719c1a
wijdan alHarbi <wijdane.ali@gmail.com>
2022-10-12 00:27:35 +0300

    function lab

--- LAB_FUNCTIONS_1.py
+++ LAB_FUNCTIONS_1.py
@@ -1,11 +1,12 @@
 
 c =int (input("please inter your number:"))
 
-def fu (count :int)->int :
+def pyramid (count :int)->int :
     '''This function type numbers in pyramid shape'''
     for i in range(0,count + 1):
         for j in range(count- i, 0, -1):
             print(j,end=' ')
         
         print()
-fu(c)
+pyramid(c)
+print (pyramid.__doc__ )
