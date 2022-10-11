de7c71a1e15c5db0f07f8bf0ccffdf3f0aaf5113
ghadah <ghadahsabharbi@gmail.com>
2022-10-11 23:36:10 +0300

    lab 3 print in descending order

--- lab3-functions1.py
+++ lab3-functions1.py
@@ -5,19 +5,17 @@ def num_pyramid (num:int):
     '''this function takes 1 prameter of integer type and prints out the result in a nice pyramid shape '''
 
     for n in range(num, 0, -1):
-        for k in range(1, n+1):
+        for k in reversed(range(1, n+1)):
             print(k, end=" ") 
 
         print("\n")
                
 
 '''
-The outer loop counts how many numbers should be printed in one line 
-the inner loop will print what number should be printed 
-
-first we say for every integer (n) in the range from the number the user enteres to 0 , and every time you repet the loop take a step behind until you reach 0 
-then for every number in the range from 1 to n (the number the user enters), print the numbers in one line with a space between them 
+first for every integer (n) in the range from the number the user enteres to 0 , and every time you repet the loop take a step behind until you reach 0 
+then for every number in the range between 1 and n+1 , print the numbers in descending order in one line with a space between them 
 ** end="" will add a white space after the number and will not print a newline 
+** reversed() will print the numbers in descending order 
 finally after every round print a new line 
 ''' 
             
