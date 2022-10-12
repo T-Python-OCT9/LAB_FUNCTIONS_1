fb7f3581946e6e43d915e2093a1d788d9c8be814
saud <Saudalotibi79@gmail.com>
2022-10-12 14:00:54 +0300

    solve function lab

---
+++ function_lab.py
@@ -0,0 +1,11 @@
+def numberloop(number: int) -> int:  # create function with one parameter type int and return int
+
+    ''' this function print the numbers in descending pattern'''  # create function document
+    
+    for num in range(number, 0 ,-1):  # for loop from the number to 1
+        print(*range(num,0,-1),sep='-> ') # printing numbers from num to 1 in oneline
+     
+
+numberloop(7)  # calling the function
+
+print(numberloop.__doc__) # printin the function documentation
