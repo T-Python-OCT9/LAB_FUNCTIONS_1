5a5701e6def2dc1b6d9175b1b0beed07959eac02
moath alosaimi <moathalosimie@hotmail.com>
2022-10-12 14:01:26 +0300

    solved the functions lab

---
+++ Functions.py
@@ -0,0 +1,24 @@
+
+
+
+def pattrens_m1(Num : int) :
+    ''' this code is to build a Pattern '''
+    for s in range (Num):
+
+        for j in range(Num-s-1,-1,-1):
+
+            print(j+1,end=" ")
+
+
+        
+
+        print ()
+         
+            
+            
+pattrens_m1 (10)
+print(pattrens_m1)
+
+
+    
+    
