a30dfb92df4b9613bf2312c23a1e749a6bb177d0
Najd <115422082+NajdGhamdi@users.noreply.github.com>
2022-10-12 16:53:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ for i in range(rows + 1, 0, -1):
 def rows():
     print('My first function This row prints the result formatted as the existing pattern ')
 rows()
+
+def rows(a: int)->int:
+    '''My first function '''
+    for i in range(a, 0 , -1):
+      for j in range(i, 0, -1):
+        print(j, end=' ')
+
+
+      print()
+
+rows(5)
+print(rows.__doc__)
